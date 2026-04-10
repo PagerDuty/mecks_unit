9d703d430f2681df19b886374cfe6b93d4d5a799
Mass Refactor Framework <mass-refactor-framework@pagerduty.com>
2026-04-10 02:18:57 +0000

    (Upgrade Elixir to 1.19 and Erlang to 28) [ast.ast_grep] If hackney is somehow being used, add ssl_verify_fun to mix.exs dependencies with rebar3 and override options to fix compatibility issues with Elixir 1.18+

--- test_app/mix.exs
+++ test_app/mix.exs
@@ -28,6 +28,7 @@ defmodule TestApp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ssl_verify_fun, "~> 1.1.0", manager: :rebar3, override: true},
       {:excoveralls, "~> 0.10", only: :test},
       {:mecks_unit, path: ".."}
     ]
